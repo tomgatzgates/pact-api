a6fbe919223cb4c678356899d114999fe6ec7674
Iestyn Williams <iest@me.com>
2015-09-07 17:34:08 +0100

    Change HTTPRequestable to use basic auth

--- modules/HTTPRequestable.js
+++ modules/HTTPRequestable.js
@@ -1,18 +1,17 @@
 import request from 'superagent';
 
 export default class HTTPRequestable {
-  // Private API: convenience request methods
   _get(url, callback) {
     const req = request.get(url);
-    if (this.accessToken) {
-      req.set('Authorization', this.accessToken);
+    if (this.token) {
+      req.auth(this.token, '');
     }
     req.end(callback);
   }
   _post(url, payload, callback) {
     const req = request.post(url);
-    if (this.accessToken) {
-      req.set('Authorization', this.accessToken);
+    if (this.token) {
+      req.auth(this.token, '');
     }
     req.type('form')
       .send(payload)
@@ -20,16 +19,16 @@ export default class HTTPRequestable {
   }
   _put(url, payload, callback) {
     const req = request.put(url);
-    if (this.accessToken) {
-      req.set('Authorization', this.accessToken);
+    if (this.token) {
+      req.auth(this.token, '');
     }
     req.send(payload)
       .end(callback);
   }
   _del(url, callback) {
     const req = request.del(url);
-    if (this.accessToken) {
-      req.set('Authorization', this.accessToken);
+    if (this.token) {
+      req.auth(this.token, '');
     }
     req.end(callback);
   }
