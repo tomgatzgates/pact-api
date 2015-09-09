5bda10504f30e8d39464289435318872f9371c07
Iestyn Williams <iest@me.com>
2015-09-09 14:56:33 +0100

    HTTPRequestable test

--- test/HTTPRequestable-test.js
+++ test/HTTPRequestable-test.js
@@ -3,7 +3,8 @@ import superagent from 'superagent';
 
 const fakeUrl = 'The human torch was denied a bank loan';
 const fakeToken = 'The arsonist had oddly shaped feet';
-const callback = sinon.spy();
+const fakePayload = 'Unique New York';
+const fakeCallback = sinon.spy();
 
 describe('HTTPRequestable', () => {
 
@@ -31,53 +32,117 @@ describe('HTTPRequestable', () => {
     sendSpy.reset();
     authSpy.reset();
     endSpy.reset();
+    fakeCallback.reset();
   });
 
   describe('_get', () => {
-
-    it(`Call's superagent's get method with the given URL`, () => {
+    it(`GETs the given URL`, () => {
       instance._get(fakeUrl);
       assert.ok(superagent.get.calledOnce);
       assert.ok(superagent.get.calledWith(fakeUrl));
     });
 
-    it(`Call's superagent's end method with the passed callback`, () => {
-      instance._get(fakeUrl, callback);
-      assert.ok(endSpy.calledWith(callback));
-    });
-
-    it(`Call's superagent's auth method when a token has been set`, () => {
+    it(`Sets authentication when a token is present`, () => {
       instance.setToken(fakeToken);
-      instance._get(fakeUrl, callback);
+      instance._get(fakeUrl, fakeCallback);
       assert.ok(authSpy.calledWith(fakeToken, ''));
     });
 
-    it('Does not call the auth method when there is no token', () => {
-      instance._get(fakeUrl, callback);
+    it(`Does not use auth when there's no token present`, () => {
+      instance._get(fakeUrl, fakeCallback);
       assert.notOk(authSpy.calledOnce);
     });
 
+    it(`Has it's callback called when the request ends`, () => {
+      instance._get(fakeUrl, fakeCallback);
+      assert.ok(endSpy.calledWith(fakeCallback));
+    });
   });
 
   describe('_post', () => {
-
-    it(`Call's superagent's post method with the given URL`, () => {
+    it(`POSTs to the given URL`, () => {
       instance._post(fakeUrl);
       assert.ok(superagent.post.calledOnce);
       assert.ok(superagent.post.calledWith(fakeUrl));
     });
 
-    it(`Call's superagent's auth method when a token has been set`, () => {
+    it(`Sets authentication when a token is present`, () => {
       instance.setToken(fakeToken);
       instance._post(fakeUrl);
       assert.ok(authSpy.calledWith(fakeToken, ''));
     });
 
-    it('Does not call the auth method when there is no token', () => {
-      instance._get(fakeUrl, callback);
+    it(`Does not use auth when there's no token present`, () => {
+      instance._post(fakeUrl);
       assert.notOk(authSpy.calledOnce);
     });
 
+    it('Sets the payload type to "form"', () => {
+      instance._post(fakeUrl);
+      assert.ok(typeSpy.calledWith('form'));
+    });
+
+    it('Sends the given payload', () => {
+      instance._post(fakeUrl, fakePayload);
+      assert.ok(sendSpy.calledWith(fakePayload));
+    });
+
+    it(`Has it's callback called when the request ends`, () => {
+      instance._post(fakeUrl, fakePayload, fakeCallback);
+      assert.ok(endSpy.calledWith(fakeCallback));
+    });
   });
 
+  describe('_put', () => {
+    it(`PUTs to the given URL`, () => {
+      instance._put(fakeUrl);
+      assert.ok(superagent.put.calledOnce);
+      assert.ok(superagent.put.calledWith(fakeUrl));
+    });
+
+    it(`Sets authentication when a token is present`, () => {
+      instance.setToken(fakeToken);
+      instance._post(fakeUrl);
+      assert.ok(authSpy.calledWith(fakeToken, ''));
+    });
+
+    it(`Does not use auth when there's no token present`, () => {
+      instance._post(fakeUrl);
+      assert.notOk(authSpy.calledOnce);
+    });
+
+    it('Sends the given payload', () => {
+      instance._post(fakeUrl, fakePayload);
+      assert.ok(sendSpy.calledWith(fakePayload));
+    });
+
+    it(`Has it's callback called when the request ends`, () => {
+      instance._post(fakeUrl, fakePayload, fakeCallback);
+      assert.ok(endSpy.calledWith(fakeCallback));
+    });
+  });
+
+  describe('_del', () => {
+    it(`DELETEs at the given URL`, () => {
+      instance._del(fakeUrl);
+      assert.ok(superagent.del.calledOnce);
+      assert.ok(superagent.del.calledWith(fakeUrl));
+    });
+
+    it(`Sets authentication when a token is present`, () => {
+      instance.setToken(fakeToken);
+      instance._post(fakeUrl);
+      assert.ok(authSpy.calledWith(fakeToken, ''));
+    });
+
+    it(`Does not use auth when there's no token present`, () => {
+      instance._post(fakeUrl);
+      assert.notOk(authSpy.calledOnce);
+    });
+
+    it(`Has it's callback called when the request ends`, () => {
+      instance._post(fakeUrl, fakePayload, fakeCallback);
+      assert.ok(endSpy.calledWith(fakeCallback));
+    });
+  });
 });
