d86ac87e9bd22dd0e3c482b6ce70752e219a4f5e
Iestyn Williams <iest@me.com>
2015-09-23 10:40:41 +0100

    Add support for url params

--- modules/PactMethod.js
+++ modules/PactMethod.js
@@ -1,10 +1,42 @@
-// This fn to be used from inside a PactResource class/subclass
-export default function pactMethod({method = 'get', path = ''}) {
-  // TODO: urlParam munging
+import {makeURLInterpolator} from './util';
+import methods from './methods';
+
+// This fn to be used from inside a PactResource class/subclass only
+export default function pactMethod({
+  method = methods.GET,
+  path = '',
+  urlParams = [],
+}) {
+  if (!methods[method]) {
+    throw new Error(
+      `PactAPI: No method exists for '${method}'. Your options are: ` +
+      Object.keys(methods)
+    );
+  }
+
+  const pathGenerator = makeURLInterpolator(path);
 
   return function makeRequest(payload) {
+    const urlData = {};
+
+    // Check to see if we have all the arguments we require
+    urlParams.forEach(param => {
+      const required = payload[param];
+      if (typeof required === 'undefined' || required === null) {
+        throw new Error(
+          `PactAPI: I require argument "${param}", but I got: ${required}`
+        );
+      }
+
+      // Add it to the url data
+      urlData[param] = required;
+
+      // Remove it from the payload
+      delete payload[param];
+    });
+
     const resourcePath = this.path;
-    const fullPath = resourcePath + path;
+    const fullPath = resourcePath + pathGenerator(urlData);
     return this._request(method, fullPath, payload);
   };
 }
